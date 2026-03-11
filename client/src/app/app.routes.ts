d1e8fa829c6151e9d795205972d86f61d0f1503f
farrahpepino <idcfarrah@gmail.com>
2026-03-11 19:27:17 -0400

    login set up

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Login } from '../Components/Auth/login/login';
+import { Home } from '../Components/Main/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Login },
+    { path: 'home', component: Home }
+];
