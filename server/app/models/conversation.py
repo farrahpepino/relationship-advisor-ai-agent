d648177177ed3fb50c5805d64bfbc8a98bdbd6a7
farrahpepino <idcfarrah@gmail.com>
2026-03-19 12:33:53 -0400

    model for message and conversation

---
+++ server/app/models/conversation.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, String, Integer, TIMESTAMP, func
+from app.database.base import Base
+
+
+class Conversation(Base):
+    __tablename__ = "conversations"
+
+    id = Column(String(255), primary_key=True, unique=True)
+    user_id = Column(String(255), nullable=False)
+    title = Column(String(255), nullable=False)
+    last_opened = Column(TIMESTAMP, server_default=func.now(), onupdate=func.now())
---
+++ server/app/models/message.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, TIMESTAMP, func
+from app.database.base import Base
+
+class Message(Base):
+    __tablename__ = "messages"
+    id = Column(String(255), primary_key=True, unique=True)
+    conversation_id = Column(String(255), nullable=False)
+    content = Column(String(1000), nullable=False)
+    role = Column(String(255), nullable=False)
+    
