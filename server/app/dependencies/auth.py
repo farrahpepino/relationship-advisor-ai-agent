dc69138a0ed0b98fa82ec58fa62c873a6df7d09a
farrahpepino <idcfarrah@gmail.com>
2026-03-19 20:07:50 -0400

    c

--- server/app/dependencies/auth.py
+++ server/app/dependencies/auth.py
@@ -7,8 +7,9 @@ from app.services.auth_service import Auth_Service
 
 auth_service = Auth_Service()
 
-def get_user(
-    token: str,
-    db: Session = Depends(get_db)
-):
-    return auth_service.authenticate_account(db, token)
+class Auth:
+    def get_user(
+        token: str,
+        db: Session = Depends(get_db)
+    ):
+        return auth_service.authenticate_account(db, token)
